0695571713b9c8ad5910dc8bf2f79a57799b683a
Theodore Kamma <iitheo@yahoo.com>
2024-08-25 19:50:28 +0100

    update permissison

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.authentication import TokenAuthentication
 from profiles_api import serializers, models, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 
 
@@ -101,7 +102,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name", "email",)
 
@@ -109,3 +110,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
